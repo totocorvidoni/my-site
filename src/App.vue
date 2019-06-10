6b8f8e2aebfab79b046be22048cee1c40bee82d0
Toto Corvidoni <totocorvidoni@gmail.com>
2019-06-10 13:28:32 -0300

    Add Hero Component.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,39 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <hero-comp/>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import HeroComp from "@/components/Hero.vue";
 
 export default {
   name: "app",
-  components: {
-    HelloWorld
-  }
+  components: { HeroComp }
 };
 </script>
 
 <style lang="scss">
+@import url("https://fonts.googleapis.com/css?family=Work+Sans:300,400,400i,700&display=swap");
+
+* {
+  box-sizing: border-box;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p {
+  margin: 0;
+}
+
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: grid;
+  grid-template-rows: 100vh;
+  font-family: $base-font;
+  color: $grey;
 }
 </style>
---
+++ src/components/Hero.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="hero">
+    <h1 class="big">Hi,</h1>
+    <h2>I'm a full-stack web developer.</h2>
+    <h3 class="likes">I like to...</h3>
+    <p>code efficently</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "hero-comp"
+};
+</script>
+
+<style lang="scss">
+#hero {
+  display: grid;
+  grid-gap: 0.5em;
+  place-content: center;
+  justify-items: center;
+
+  .big {
+    font-size: 5em;
+    margin: 0;
+  }
+
+  .likes {
+    margin-top: 3em;
+  }
+}
+</style>
