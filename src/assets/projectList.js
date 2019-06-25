91ba14fa80fd9ccbaba791b075f9d7eb7a13cd11
Toto Corvidoni <totocorvidoni@gmail.com>
2019-06-25 17:40:30 -0300

    Add myLibrary to Projects.

--- src/assets/projectList.js
+++ src/assets/projectList.js
@@ -6,8 +6,8 @@ export default [
       live: "https://totocorvidoni.github.io/todo-vue/"
     },
     description:
-      "A front end app made with Vue and my take on todo lists. Taking advantage of the simplicity of a todo manager I payed special attention on refining and integrating additional features such as, priorities and due dates. It allowed me to mainly focus on logic and functions to manipulate and present data.",
-    tags: ["Todo", "Vue", "Vuex"],
+      "Made with Vue, it's my take on todo lists. Taking advantage of the simplicity of a todo manager I payed special attention on refining and integrating additional features such as, priorities and due dates. It allowed me to mainly focus on logic and functions to manipulate and present data.",
+    tags: ["Todo", "Vue", "Vuex", "Sass"],
     images: [
       {
         thumb: require("@/assets/images/projects/todo/01-thumb.png"),
@@ -22,44 +22,44 @@ export default [
       {
         thumb: require("@/assets/images/projects/todo/03-thumb.png"),
         full: require("@/assets/images/projects/todo/03.png"),
-        alt: "placeholder alternate text."
+        alt: "A full view with no project opened."
       },
       {
         thumb: require("@/assets/images/projects/todo/04-thumb.png"),
         full: require("@/assets/images/projects/todo/04.png"),
-        alt: "placeholder alternate text."
+        alt: "A full view with a project and a todo selected."
       }
     ]
   },
   {
-    name: "Placeholder Project 2",
+    name: "My Library",
     links: {
-      repo: "https://github.com/totocorvidoni",
-      live: "https://github.com/totocorvidoni"
+      repo: "https://github.com/totocorvidoni/myLibrary",
+      live: "https://totocorvidoni.github.io/myLibrary/"
     },
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut soluta eligendi, autem tempore sed quisquam, deleniti exercitationem deserunt dolore provident rerum maxime doloribus laboriosam excepturi adipisci?text= Iure placeat officiis maiores?text=",
-    tags: ["tag1", "tag two", "longer tag", "tag4"],
+      "It lets you add books to a virtual library, allowing you to keep track of the read status. The best part of the project was figuring out how to implement all the needed features into the book cards. I wanted each element to feel natural on them and that forced me to find creative solutions for their implementations.",
+    tags: ["Books", "Vue", "CSS3"],
     images: [
       {
-        thumb: "https://via.placeholder.com/300x300?text=2",
-        full: "https://via.placeholder.com/200x400?text=2",
-        alt: "placeholder alternate text."
+        thumb: require("@/assets/images/projects/library/01-thumb.png"),
+        full: require("@/assets/images/projects/library/01.png"),
+        alt: "Thou Shall Do logo."
       },
       {
-        thumb: "https://via.placeholder.com/150x300?text=1",
-        full: "https://via.placeholder.com/450x900?text=1",
-        alt: "placeholder alternate text."
+        thumb: require("@/assets/images/projects/library/02-thumb.png"),
+        full: require("@/assets/images/projects/library/02.png"),
+        alt: "Quick mode todos."
       },
       {
-        thumb: "https://via.placeholder.com/100x900?text=3",
-        full: "https://via.placeholder.com/900x500?text=3",
-        alt: "placeholder alternate text."
+        thumb: require("@/assets/images/projects/library/03-thumb.png"),
+        full: require("@/assets/images/projects/library/03.png"),
+        alt: "A full view with no project opened."
       },
       {
-        thumb: "https://via.placeholder.com/450x700?text=4",
-        full: "https://via.placeholder.com/380x120?text=4",
-        alt: "placeholder alternate text."
+        thumb: require("@/assets/images/projects/library/04-thumb.png"),
+        full: require("@/assets/images/projects/library/04.png"),
+        alt: "A full view with a project and a library selected."
       }
     ]
   },
