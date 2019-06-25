839d7508e2994ec9ff1ae379b3158da575b77eec
Toto Corvidoni <totocorvidoni@gmail.com>
2019-06-25 13:50:33 -0300

    Add Todo Project.

--- src/assets/projectList.js
+++ src/assets/projectList.js
@@ -1,32 +1,32 @@
 export default [
   {
-    name: "Placeholder Project 1",
+    name: "Thou Shall Do",
     links: {
-      repo: "https://github.com/totocorvidoni",
-      live: "https://github.com/totocorvidoni"
+      repo: "https://github.com/totocorvidoni/todo-vue",
+      live: "https://totocorvidoni.github.io/todo-vue/"
     },
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut soluta eligendi, autem tempore sed quisquam, deleniti exercitationem deserunt dolore provident rerum maxime doloribus laboriosam excepturi adipisci?text= Iure placeat officiis maiores?",
-    tags: ["tag1", "tag two", "longer tag", "tag4"],
+      "A front end app made with Vue and my take on todo lists. Taking advantage of the simplicity of a todo manager I payed special attention on refining and integrating additional features such as, priorities and due dates. It allowed me to mainly focus on logic and functions to manipulate and present data.",
+    tags: ["Todo", "Vue", "Vuex"],
     images: [
       {
-        thumb: "https://via.placeholder.com/150x300?text=1",
-        full: "https://via.placeholder.com/450x900?text=1",
+        thumb: require("@/assets/images/projects/todo/01-thumb.png"),
+        full: require("@/assets/images/projects/todo/01.png"),
+        alt: "Thou Shall Do logo."
+      },
+      {
+        thumb: require("@/assets/images/projects/todo/02-thumb.png"),
+        full: require("@/assets/images/projects/todo/02.png"),
+        alt: "Quick mode todos."
+      },
+      {
+        thumb: require("@/assets/images/projects/todo/03-thumb.png"),
+        full: require("@/assets/images/projects/todo/03.png"),
         alt: "placeholder alternate text."
       },
       {
-        thumb: "https://via.placeholder.com/300x300?text=2",
-        full: "https://via.placeholder.com/200x400?text=2",
-        alt: "placeholder alternate text."
-      },
-      {
-        thumb: "https://via.placeholder.com/100x900?text=3",
-        full: "https://via.placeholder.com/900x500?text=3",
-        alt: "placeholder alternate text."
-      },
-      {
-        thumb: "https://via.placeholder.com/450x700?text=4",
-        full: "https://via.placeholder.com/380x120?text=4",
+        thumb: require("@/assets/images/projects/todo/04-thumb.png"),
+        full: require("@/assets/images/projects/todo/04.png"),
         alt: "placeholder alternate text."
       }
     ]
