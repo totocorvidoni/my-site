2f3a1eaf3a9891fc472d3bbf3f8de7ff0a38d375
Toto Corvidoni <totocorvidoni@gmail.com>
2019-06-26 09:42:27 -0300

    Clean up Project list.

--- src/assets/projectList.js
+++ src/assets/projectList.js
@@ -7,7 +7,7 @@ export default [
     },
     description:
       "Made with Vue, it's my take on todo lists. Taking advantage of the simplicity of a todo manager I payed special attention on refining and integrating additional features such as, priorities and due dates. It allowed me to mainly focus on logic and functions to manipulate and present data.",
-    tags: ["Todo", "Vue", "Vuex", "Sass"],
+    tags: ["Todo", "Vue", "Vuex", "Sass", "Front End"],
     images: [
       {
         thumb: require("@/assets/images/projects/todo/01-thumb.png"),
@@ -39,7 +39,7 @@ export default [
     },
     description:
       "It lets you add books to a virtual library, allowing you to keep track of the read status. The best part of the project was figuring out how to implement all the needed features into the book cards. I wanted each element to feel natural on them and that forced me to find creative solutions for their implementations.",
-    tags: ["Books", "Vue", "CSS3"],
+    tags: ["Books", "Vue", "CSS3", "Front End"],
     images: [
       {
         thumb: require("@/assets/images/projects/library/01-thumb.png"),
@@ -60,134 +60,6 @@ export default [
         thumb: require("@/assets/images/projects/library/04-thumb.png"),
         full: require("@/assets/images/projects/library/04.png"),
         alt: "A full view with a project and a library selected."
-      }
-    ]
-  },
-  {
-    name: "Placeholder Project 3",
-    links: {
-      repo: "https://github.com/totocorvidoni",
-      live: "https://github.com/totocorvidoni"
-    },
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut soluta eligendi, autem tempore sed quisquam, deleniti exercitationem deserunt dolore provident rerum maxime doloribus laboriosam excepturi adipisci?text= Iure placeat officiis maiores?text=",
-    tags: ["tag1", "tag two", "longer tag", "tag4"],
-    images: [
-      {
-        thumb: "https://via.placeholder.com/100x900?text=3",
-        full: "https://via.placeholder.com/900x500?text=3",
-        alt: "placeholder alternate text."
-      },
-      {
-        thumb: "https://via.placeholder.com/150x300?text=1",
-        full: "https://via.placeholder.com/450x900?text=1",
-        alt: "placeholder alternate text."
-      },
-      {
-        thumb: "https://via.placeholder.com/300x300?text=2",
-        full: "https://via.placeholder.com/200x400?text=2",
-        alt: "placeholder alternate text."
-      },
-      {
-        thumb: "https://via.placeholder.com/450x700?text=4",
-        full: "https://via.placeholder.com/380x120?text=4",
-        alt: "placeholder alternate text."
-      }
-    ]
-  },
-  {
-    name: "Placeholder Project 4",
-    links: {
-      repo: "https://github.com/totocorvidoni",
-      live: "https://github.com/totocorvidoni"
-    },
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut soluta eligendi, autem tempore sed quisquam, deleniti exercitationem deserunt dolore provident rerum maxime doloribus laboriosam excepturi adipisci?text= Iure placeat officiis maiores?text=",
-    tags: ["tag1", "tag two", "longer tag", "tag4"],
-    images: [
-      {
-        thumb: "https://via.placeholder.com/450x700?text=4",
-        full: "https://via.placeholder.com/380x120?text=4",
-        alt: "placeholder alternate text."
-      },
-      {
-        thumb: "https://via.placeholder.com/150x300?text=1",
-        full: "https://via.placeholder.com/450x900?text=1",
-        alt: "placeholder alternate text."
-      },
-      {
-        thumb: "https://via.placeholder.com/300x300?text=2",
-        full: "https://via.placeholder.com/200x400?text=2",
-        alt: "placeholder alternate text."
-      },
-      {
-        thumb: "https://via.placeholder.com/100x900?text=3",
-        full: "https://via.placeholder.com/900x500?text=3",
-        alt: "placeholder alternate text."
-      }
-    ]
-  },
-  {
-    name: "Placeholder Project 5",
-    links: {
-      repo: "https://github.com/totocorvidoni",
-      live: "https://github.com/totocorvidoni"
-    },
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut soluta eligendi, autem tempore sed quisquam, deleniti exercitationem deserunt dolore provident rerum maxime doloribus laboriosam excepturi adipisci?text= Iure placeat officiis maiores?text=",
-    tags: ["tag1", "tag two", "longer tag", "tag4"],
-    images: [
-      {
-        thumb: "https://via.placeholder.com/150x300?text=1",
-        full: "https://via.placeholder.com/450x900?text=1",
-        alt: "placeholder alternate text."
-      },
-      {
-        thumb: "https://via.placeholder.com/300x300?text=2",
-        full: "https://via.placeholder.com/200x400?text=2",
-        alt: "placeholder alternate text."
-      },
-      {
-        thumb: "https://via.placeholder.com/100x900?text=3",
-        full: "https://via.placeholder.com/900x500?text=3",
-        alt: "placeholder alternate text."
-      },
-      {
-        thumb: "https://via.placeholder.com/450x700?text=4",
-        full: "https://via.placeholder.com/380x120?text=4",
-        alt: "placeholder alternate text."
-      }
-    ]
-  },
-  {
-    name: "Placeholder Project 6",
-    links: {
-      repo: "https://github.com/totocorvidoni",
-      live: "https://github.com/totocorvidoni"
-    },
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut soluta eligendi, autem tempore sed quisquam, deleniti exercitationem deserunt dolore provident rerum maxime doloribus laboriosam excepturi adipisci?text= Iure placeat officiis maiores?text=",
-    tags: ["tag1", "tag two", "longer tag", "tag4"],
-    images: [
-      {
-        thumb: "https://via.placeholder.com/150x300?text=1",
-        full: "https://via.placeholder.com/450x900?text=1",
-        alt: "placeholder alternate text."
-      },
-      {
-        thumb: "https://via.placeholder.com/300x300?text=2",
-        full: "https://via.placeholder.com/200x400?text=2",
-        alt: "placeholder alternate text."
-      },
-      {
-        thumb: "https://via.placeholder.com/100x900?text=3",
-        full: "https://via.placeholder.com/900x500?text=3",
-        alt: "placeholder alternate text."
-      },
-      {
-        thumb: "https://via.placeholder.com/450x700?text=4",
-        full: "https://via.placeholder.com/380x120?text=4",
-        alt: "placeholder alternate text."
       }
     ]
   }
