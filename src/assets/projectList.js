a1d9ae8d1dd086521c921c59ecfddc9586c03de4
Toto Corvidoni <totocorvidoni@gmail.com>
2019-07-22 11:08:19 -0300

    Add Crafts Store to projects.

--- src/assets/projectList.js
+++ src/assets/projectList.js
@@ -1,5 +1,38 @@
 export default [
   {
+    name: "Pupe Lepú",
+    links: {
+      repo: "https://github.com/totocorvidoni/vue-crafts-store",
+      live: "https://tomascorridoni.com/pupe-lepu/"
+    },
+    description:
+      "<p>An online catalog for hand made products.</p> <p>The site was designed focusing on displaying the client's products in detail while at the same time letting her personality show.</p> <p>The admin section has been setup with Wordpress and uses WooCommerce for product management, this information is sent to the front end app through the WooCommerce REST API.</p>",
+    tags: ["Store", "Vue", "Wordpress", "WooCommerce", "API"],
+    images: [
+      {
+        thumb: require("@/assets/images/projects/lepu/01-thumb.jpg"),
+        full: require("@/assets/images/projects/lepu/01.jpg"),
+        alt: "Pupe Lepú logo, a rabbit in a grey circle."
+      },
+      {
+        thumb: require("@/assets/images/projects/lepu/02-thumb.jpg"),
+        full: require("@/assets/images/projects/lepu/02.jpg"),
+        alt: "Several products placed in cards."
+      },
+      {
+        thumb: require("@/assets/images/projects/lepu/03-thumb.jpg"),
+        full: require("@/assets/images/projects/lepu/03.jpg"),
+        alt:
+          "The picture of a fox amigurumi leaning on a book and it's description to the right."
+      },
+      {
+        thumb: require("@/assets/images/projects/lepu/04-thumb.jpg"),
+        full: require("@/assets/images/projects/lepu/04.jpg"),
+        alt: "An admin panel showing product details."
+      }
+    ]
+  },
+  {
     name: "Thou Shall Do",
     links: {
       repo: "https://github.com/totocorvidoni/todo-vue",
@@ -7,7 +40,7 @@ export default [
     },
     description:
       "<p>Made with Vue, it's my take on todo lists. </p><p> Taking advantage of the simplicity of a todo manager I payed special attention on refining and integrating additional features such as, priorities and due dates. It allowed me to mainly focus on logic and functions to manipulate and present data.</p>",
-    tags: ["Todo", "Vue", "Vuex", "Sass", "Front End"],
+    tags: ["Todo", "Vue", "Vuex", "Sass"],
     images: [
       {
         thumb: require("@/assets/images/projects/todo/01-thumb.png"),
@@ -39,7 +72,7 @@ export default [
     },
     description:
       "<p>It lets you add books to a virtual library, allowing you to keep track of the read status.</p> <p>The best part of the project was figuring out how to implement all the needed features into the book cards. I wanted each element to feel natural on them and that forced me to find creative solutions for their implementations.</p>",
-    tags: ["Books", "Vue", "Front End"],
+    tags: ["Books", "Vue"],
     images: [
       {
         thumb: require("@/assets/images/projects/library/01-thumb.png"),
@@ -71,7 +104,7 @@ export default [
     },
     description:
       "<p>Play \"Where's Wally?\" online! </p><p> On the front end we have a Vue app that let's you navigate the scene to find hidden characters, on the back end there is a Rails API that validates guesses and saves High Scores. </p><p> Figuring out the best way to validate guesses was my favorite part, my least favorite was finding Woof.</p>",
-    tags: ["Game", "Vue", "Rails", "Front End", "Back End", "API"],
+    tags: ["Game", "Vue", "Rails", "API"],
     images: [
       {
         thumb: require("@/assets/images/projects/wally/01-thumb.jpg"),
