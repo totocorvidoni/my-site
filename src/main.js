ca68fa86411c3d24e75f867947d74fade807fa96
Toto Corvidoni <totocorvidoni@gmail.com>
2019-07-29 11:08:22 -0300

    Add vue-i18n.

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import VeeValidate from "vee-validate";
 import App from "./App.vue";
 import "normalize.css";
+import i18n from "./i18n";
 
 Vue.use(VeeValidate, {
   classes: true
@@ -10,5 +11,6 @@ Vue.use(VeeValidate, {
 Vue.config.productionTip = false;
 
 new Vue({
+  i18n,
   render: h => h(App)
 }).$mount("#app");
--- vue.config.js
+++ vue.config.js
@@ -5,5 +5,14 @@ module.exports = {
         data: '@import "@/scss/_variables.scss";'
       }
     }
+  },
+
+  pluginOptions: {
+    i18n: {
+      locale: "en",
+      fallbackLocale: "en",
+      localeDir: "locales",
+      enableInSFC: false
+    }
   }
 };
